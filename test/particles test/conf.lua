2bebf5ec99c93171819e7cc4dea9c6f0c69d3e91
Thomas Smallridge <Thomas@studentedge.com.au>
2017-04-20 12:23:17 +0800

    particles test works and stuff its pretty ok

---
+++ test/particles test/conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+    t.window.width = 200
+	t.window.height = 200
+    t.title = "cool things"
+end
--- test/particles test/main.lua
+++ test/particles test/main.lua
@@ -1,18 +1,32 @@
-local particleSystem = nil
+local psystem = nil
 
 
 function love.load()
     local img = love.graphics.newImage('effect.png')
-    particleSystem = love.graphics.newParticleSystem(img, 10)
-    particleSystem:setEmissionRate(5)
-    particleSystem:setParticleLifetime(3, 5)
-    particleSystem:setSpread(5)
+    psystem = love.graphics.newParticleSystem(img, 64)
+    psystem:setQuads(love.graphics.newQuad(0, 32, 16, 16, img:getDimensions()), love.graphics.newQuad(16, 32, 16, 16, img:getDimensions()),
+    love.graphics.newQuad(32, 32, 16, 16, img:getDimensions()), love.graphics.newQuad(48, 32, 16, 16, img:getDimensions()))
+    psystem:setEmissionRate(10)
+    psystem:setLinearAcceleration(-50, -50, 50, 50) -- Randomized movement towards the bottom of the screen.
+    psystem:setRotation(0, math.pi)
+    psystem:setParticleLifetime(0.5, 2)
 end
 
 function love.update(dt)
-    particleSystem:update(dt)
+    psystem:update(dt)
 end
 
 function love.draw()
-    love.graphics.draw(particleSystem, 100, 100)
+    love.graphics.draw(psystem, love.graphics.getWidth()/2, love.graphics.getHeight()/2)
+    love.graphics.print("press space if ur cool", 10, love.graphics.getHeight()-30)
+end
+
+function love.keypressed(key)
+	if key == 'space' then
+        if psystem:isPaused() then
+            psystem:start()
+        else
+            psystem:pause()
+        end
+	end
 end
